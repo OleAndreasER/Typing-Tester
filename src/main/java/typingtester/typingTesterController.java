cb0dc294f890e18a7026c10bd080813c597b953b
OleAndreasER <oarorvik@ntnu.no>
2022-04-10 07:57:49 +0200

    nothing

--- src/main/java/typingtester/typingTesterController.java
+++ src/main/java/typingtester/typingTesterController.java
@@ -4,7 +4,6 @@ package typingtester;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
 
 public class TypingTesterController {
     @FXML
