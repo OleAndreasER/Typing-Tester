dbdf6feb1fa34f8e58b3436dad4c6f38abc401f2
OleAndreasER <oleaegeland@gmail.com>
2022-04-13 10:27:22 +0200

    Word skip handling for space and erasing space

--- src/main/java/typingtester/TypingTest.java
+++ src/main/java/typingtester/TypingTest.java
@@ -13,20 +13,24 @@ public class TypingTest {
 
     public void type(String c) {
         typed += c;
-        //"ole and|reas|land"
-        //"ole and|"
 
-        if (c.equals(" ")) {
+        if (c.equals(" "))
             caretIndex = words.indexOf(" ", caretIndex) + 1; //Jump to next word
-        }
-        else {
+            
+        else if (currentWordLengthDifference() <= 0) //caret shouldn't move over the next word if you overtype a word.
             caretIndex++;
-        }
     }
 
     public void eraseLetter() {
         if (typed.length() == 0) return;
-        caretIndex--;
+
+        //If the word is "hello", and you type "he" then " ", you would skip "llo". This makes it so you can go back to
+        //the end of "he" if you erase the " ".
+        if (typed.charAt(typed.length()-1) == ' ')
+            caretIndex += Math.min(currentWordLengthDifference()-1, -1); 
+        else if (currentWordLengthDifference() <= 0)
+            caretIndex--;
+
         typed = typed.substring(0, typed.length()-1);
     }
 
@@ -53,6 +57,19 @@ public class TypingTest {
             whitespace += " ";
 
         return whitespace + typed;
+    }
+
+    private int currentWordLengthDifference() {
+        // typed: hellowor
+        // words: hel
+        // difference: 5
+
+        String[] typedArr = typed.split(" ");
+        String[] wordsArr = words.split(" ");
+        String lastTyped = typedArr[typedArr.length - 1];
+        String correspondingWord = wordsArr[typedArr.length -1];
+
+        return lastTyped.length() - correspondingWord.length();
     }
 
 }
