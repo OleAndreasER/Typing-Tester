8c2ca443f9cedc78cfb4920b6aa3e28697944a01
OleAndreasER <oleaegeland@gmail.com>
2022-04-14 13:48:40 +0200

    Cleanup

--- src/main/java/typingtester/TypingTest.java
+++ src/main/java/typingtester/TypingTest.java
@@ -11,7 +11,8 @@ public class TypingTest {
     private String words;
     //Left side:
     private String typed = "";
-    private final int typedLength = 28;
+
+    private static final int typedDisplayLength = 28;
 
     private int caretIndex; //caret's index on words
 
@@ -28,51 +29,6 @@ public class TypingTest {
         this.seconds = seconds;
     }
 
-    public void type(String c) {
-        if (c.length() > 1)
-            throw new IllegalArgumentException("Only one character.");
-
-        typed += c;
-
-        if (c.equals(" ")) {
-            addNewWord();
-            caretIndex = words.indexOf(" ", caretIndex) + 1; //Jump to next word
-        }
-            
-        else if (currentWordLengthDifference() <= 0) //caret shouldn't move if you overtype a word.
-            caretIndex++;
-    }
-
-    private void addNewWord() {
-        words += WordGenerator.getRandomWords(1);
-    }
-
-    public void eraseLetter() {
-        if (typed.length() == 0) return;
-
-        //If the word is "hello", and you type "he" then " ", you would skip "llo".
-        //This code under places the caret back like this: "he|llo" if you erase the " ".
-        if (typed.charAt(typed.length()-1) == ' ')
-            caretIndex += Math.min(currentWordLengthDifference()-1, -1); 
-
-        else if (currentWordLengthDifference() <= 0)
-            caretIndex--;
-
-        typed = typed.substring(0, typed.length()-1);
-    }
-
-    private int currentWordLengthDifference() {
-        // typed: hellowor
-        // words: hel
-        // difference: 5
-
-        String[] typedArr = typed.split(" ");
-        String[] wordsArr = words.split(" ");
-        String lastTyped = typedArr[typedArr.length - 1];
-        String correspondingWord = wordsArr[typedArr.length -1];
-
-        return lastTyped.length() - correspondingWord.length();
-    }
 
     public float getWPM() {
         String[] typedArr = typed.split(" ");
@@ -100,13 +56,15 @@ public class TypingTest {
         return getTypedDisplay(false);
     }
 
+
+    // --Determining correct/incorrect words
     private String getTypedDisplay(boolean isCorrectWords) {
-        int missingChars = typedLength - typed.length();
+        int missingChars = typedDisplayLength - typed.length();
         
         String typedMasked = getTypedMasked(isCorrectWords);
 
         if (missingChars <= 0)
-            return typedMasked.substring(typedMasked.length() - typedLength);
+            return typedMasked.substring(typedMasked.length() - typedDisplayLength);
         
         String whitespace = "";
         for (int i = 0; i < missingChars; i++)
@@ -116,13 +74,13 @@ public class TypingTest {
     }
 
     private String getTypedMasked(boolean isCorrectWords) { // masks with " "
-        List<String> typedList = splitAfter(typed, ' ');
-        List<String> wordsList = splitAfter(words, ' ');
+        String[] typedList = splitAfter(typed, ' ');
+        String[] wordsList = splitAfter(words, ' ');
 
         String ret = "";
-        for (int i = 0; i < typedList.size(); i++) {
-            String typedWord = typedList.get(i);
-            boolean isCorrectWord = isCorrectSoFar(typedWord, wordsList.get(i));
+        for (int i = 0; i < typedList.length; i++) {
+            String typedWord = typedList[i];
+            boolean isCorrectWord = isCorrectSoFar(typedWord, wordsList[i]);
             
             ret += maskIf(typedWord, isCorrectWords ? !isCorrectWord : isCorrectWord);
         }
@@ -130,20 +88,22 @@ public class TypingTest {
     }
 
 
-    private List<String> splitAfter(String s, char separator) {
-        if (s.length() == 0) return new ArrayList<String>();
+    private String[] splitAfter(String s, char separator) {
+
+        if (s.length() == 0) return new String[0];
 
         String[] splitted = s.split(separator+"");
-        List<String> splittedList = new ArrayList<>(Arrays.asList(splitted));
-        List<String> ret = new ArrayList<>();
-        for (int i = 0; i < splittedList.size()-1; i++) {
-            ret.add(splittedList.get(i) + separator);
+        List<String> splittedList = new ArrayList<>();
+        for (int i = 0; i < splitted.length-1; i++) {
+            splittedList.add(splitted[i] + separator);
         }
-        String lastElement = splittedList.get(splittedList.size()-1);
+        String lastElement = splitted[splitted.length-1];
         char lastCharOfS = s.charAt(s.length()-1);
 
-        ret.add(lastElement + (lastCharOfS == separator ? separator : ""));
-        return ret;
+        splittedList.add(lastElement + (lastCharOfS == separator ? separator : ""));
+
+        String[] ret = new String[splittedList.size()];
+        return splittedList.toArray(ret);
     }
 
     private boolean isCorrectSoFar(String word, String solution) {
@@ -160,5 +120,52 @@ public class TypingTest {
         for (int i = 0; i < s.length(); i++)
             masked += " ";
         return masked;
+    }
+
+
+    // --Controlling the test
+    public void type(String c) {
+        if (c.length() > 1)
+            throw new IllegalArgumentException("Only one character.");
+
+        typed += c;
+
+        if (c.equals(" ")) {
+            addNewWord();
+            caretIndex = words.indexOf(" ", caretIndex) + 1; //Jumps to next word
+        }
+            
+        else if (currentWordLengthDifference() <= 0) //caret shouldn't move if you overtype a word.
+            caretIndex++;
+    }
+
+    private void addNewWord() {
+        words += WordGenerator.getRandomWords(1);
+    }
+
+    public void eraseLetter() {
+        if (typed.length() == 0) return;
+
+        //Reverses word jump
+        if (typed.charAt(typed.length()-1) == ' ')
+            caretIndex += Math.min(currentWordLengthDifference()-1, -1); 
+
+        else if (currentWordLengthDifference() <= 0)
+            caretIndex--;
+
+        typed = typed.substring(0, typed.length()-1);
+    }
+
+    private int currentWordLengthDifference() {
+        // typed: hellowor
+        // words: hel
+        // difference: 5
+
+        String[] typedArr = typed.split(" ");
+        String[] wordsArr = words.split(" ");
+        String lastTyped = typedArr[typedArr.length - 1];
+        String correspondingWord = wordsArr[typedArr.length -1];
+
+        return lastTyped.length() - correspondingWord.length();
     }
 }
