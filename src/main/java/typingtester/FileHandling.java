40bbbb2406483ea22d7f9bfe0055fc6413c992c5
OleAndreasER <oleaegeland@gmail.com>
2022-04-16 14:17:19 +0200

    Saving test to file

--- src/main/java/typingtester/FileHandling.java
+++ src/main/java/typingtester/FileHandling.java
@@ -3,21 +3,29 @@ package typingtester;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class FileHandling {
-    private static final String fileLocation = "tests.txt";
+    private static final Path filePath = Paths.get("tests.txt");
+        
 
     public static void saveTest(StatFormat statFormat) {
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(fileLocation));
-
+            String previousContent = fileContent();
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filePath.toString()));
+            writer.write(previousContent + statFormat.toString()+"\n");
             writer.close();
         }
         catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    private static String fileContent() throws IOException {
+        return Files.readString(filePath);
     }
 
     public static List<StatFormat> loadTests() {
